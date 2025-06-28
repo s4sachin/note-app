916bf3d76d7b6cbdc8e1809486703c36b5701e31
s4sachin <sachinpurohit.text@gmail.com>
2025-06-28 09:47:39 +0530

    add: db CRUD and functions

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
 #!/usr/bin/env node
 
-const note = process.argv[2];
-const newNote = {
-    note: note,
-    id: Date.now()
-}
-
-console.log(newNote);
+import './src/command.js';
---
+++ src/note.js
@@ -0,0 +1,39 @@
+import { insertDB, getDB, saveDB } from "./db.js";
+
+export const newNote = async (note, tags) => {
+  const newNote = {
+    content: note,
+    id: Date.now(),
+    tags,
+  };
+
+  await insertDB(newNote);
+  return newNote;
+};
+
+export const getAllNotes = async () => {
+  const { notes } = await getDB();
+  return notes;
+};
+
+export const findNotes = async (filter) => {
+  const { notes } = await getDB();
+  return notes.filter((note) => note.content.toLowerCase().includes(filter.toLowerCase()));
+};
+
+
+export const removeNote = async id => {
+    const {notes} = await getDB();
+    const match = notes.find((note) => note.id === id)
+
+    if(match){
+        const newNotes = notes.filter((note) => note.id !== id)
+        await saveDB({notes: newNotes})
+        return id;
+    }
+}
+
+export const removeAllNotes = () => {
+    saveDB({notes: []})
+    return true;
+}
